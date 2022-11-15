6e60fda840ae806a9681e79813bbe7f99dc0750a
cart96 <agolfcart@tuta.io>
2022-11-16 02:45:37 +0300

    fix memory leaks

--- src/fart/main.c
+++ src/fart/main.c
@@ -20,16 +20,25 @@ int main(int argc, char **argv)
 
     fart_lexer *lexer = fart_lexer_init(content);
     if (lexer == NULL)
+    {
+        free(content);
         return -1;
+    }
 
     fart_assembler *assembler = fart_assembler_init(lexer);
     if (assembler == NULL)
+    {
+        free(content);
         return -1;
+    }
 
     fart_assembler_run(assembler);
 
     write_file(output, assembler->binary, assembler->lexer->binary_size);
     printf("passed 3, wrote %lld byte.\n", assembler->lexer->binary_size);
+
+    fart_assembler_free(assembler);
+    free(content);
 
     return 0;
 }
