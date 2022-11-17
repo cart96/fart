4320afe802119d44b5eb68a11492c04e96e4b91f
cart96 <icecat696@proton.me>
2022-11-17 13:02:57 +0300

    improve bracket errors

--- src/libfart/checker.c
+++ src/libfart/checker.c
@@ -3,7 +3,7 @@
 
 int run_checker(fart_lexer *lexer, fart_token *tokens)
 {
-    size_t index = 0, temp_index = 0;
+    size_t index = 0, temp_index = 0, last_open = 0;
     fart_jump_table temp_jump_table[2048];
 
     for (;;)
@@ -15,13 +15,14 @@ int run_checker(fart_lexer *lexer, fart_token *tokens)
         }
         else if (token->kind == FART_TOKEN_LOOP_OPEN)
         {
+            last_open = index + 1;
             temp_jump_table[temp_index].end = index;
             temp_jump_table[temp_index++].start = token->value;
         }
         else if (token->kind == FART_TOKEN_LOOP_CLOSE)
         {
             if (temp_index == 0)
-                return token->value;
+                return index + 1;
 
             size_t matched_token_index = temp_jump_table[--temp_index].end;
 
@@ -34,6 +35,10 @@ int run_checker(fart_lexer *lexer, fart_token *tokens)
         }
 
         index++;
+    }
+
+    if (temp_index != 0) {
+        return last_open;
     }
 
     return -1;
